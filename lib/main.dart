27d96e453430ea09bf88673baf6e9b2d2ed01821
philip.calape <philip.calape@whitecloak.com>
2023-06-21 00:40:35 +0800

    Move backend folder

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_todos/bootstrap.dart';
 import 'package:local_storage_todos_api/local_storage_todos_api.dart';
 
+import 'backend/db_initialize/db_initialize.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  ExpensesDatabase.instance.database;
 
   final todosApi = LocalStorageTodosApi(
     plugin: await SharedPreferences.getInstance(),
